702f6d270c0a6c445a7aba58cf20c53bd81a0a9e
lixiaoyu0429 <lixiaoyu9398@gmail.com>
2024-04-18 23:53:40 +0800

    test6

--- arch/HybridNet.py
+++ arch/HybridNet.py
@@ -315,9 +315,7 @@ def MSE(t1, t2):
     return torch.mean((t1 - t2) ** 2)
 
 def MRAE(t1, t2):
-    t1[t1 == 0] = 1e-6
-    t2[t2 == 0] = 1e-6
-    return torch.mean(torch.abs(t1 - t2) / torch.abs(t1))
+    return torch.mean(torch.abs(t1 - t2) / (torch.abs(t1) + 1e-6))
     # return 0
 
 # MatchLossFcn = nn.MSELoss(reduction='mean')
